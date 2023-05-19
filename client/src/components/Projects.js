3b9ea5fc3471c0f55a5603e81ed4a256a8c4c708
Thom Clegg <tomclegg25@gmail.com>
2023-05-19 15:32:01 +0100

    projects looking good needs new font and bg

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -1,25 +1,32 @@
 import { ProjectData } from '../data/ProjectData'
+import { Fragment } from 'react'
 
 
 const Projects = () => {
 
   return (
-    <main>
+    <main className='projects'>
       <h1> Projects</h1>
-      <section>
+      <section className='project-display'>
         {ProjectData.map(project => {
           return (
-            <div key={project.id} className={project.name.replace(' ', '-').toLowerCase()}>
-              <h3> {project.name} </h3>
-              <img src= {project.gif} /> 
-              <p> {project.description}</p>
-              {project.tech.map((t, i) => {
-                return (
-                  <div key={i}>
-                    {t}
-                  </div>
-                )
-              })}
+            <div key={project.id} className={project.name.replace(' ', '-').replace('\'', '').toLowerCase()}>
+              <div className='tile'>
+                <img src={project.gif} />
+                <div>
+                  <h3> {project.name} </h3>
+                  <p> {project.description}</p>
+                </div>
+              </div>
+              <div className='tech-container' >
+                {project.tech.map((t, i) => {
+                  return (
+                    <div className='tech' key={i}>
+                      {t}
+                    </div>
+                  )
+                })}
+              </div>
             </div>
           )
         })}
--- client/src/data/ProjectData.js
+++ client/src/data/ProjectData.js
@@ -5,31 +5,31 @@ import momsSpag from '../assets/moms-spag.gif'
 
 export const ProjectData = [
   {
+    name: 'Atmos',
+    description: 'An experimental app for creating and experiencing atmosphere. Users pair an image and audio together to create the feeling of an atmosphere, whether real or imagined. They can view other atmospheres and also play with the audio using custom-built audio processing software.',
+    gif: atmos,
+    tech: ['Django','PostgreSQL','React','Python'],
+    id: 4,
+  },
+  {
     name: 'Space Invaders',
-    description: 'For our first project of the course we were asked to create a grid based game using a combination of HTML, CSS and JavaScript, and I chose Space Invaders. It was a really great challenge, requiring a deep understanding of the intervals and scope. It also allowed me to dig into the visual elements of an app, and I created my own player, aliens, and rockets all from scratch, as well as adding my own music!',
+    description: 'My version of the classic arcade game, made with my own characters and music! ',
     gif: spaceInvaders,
     tech: ['JavaScipt','CSS'],
     id: 1,
   },
   {
     name: 'Mom\'s Spaghetti',
-    description: 'At the end of Module 2, we were given the task to find an external API and create a React app that consumed it. Me and my partner found 2 APIs, one that returned a list of words that rhymed with the word sent, and another that returned a random word. The user can input a word of get a random word, and the app gives back rhymes to that word. I was proud of implementing pagination for the results, because the API didn’t support it so I had to use the map method in an interesting way to create pagination, and I loved the SASS we implemented, making the app Eminem themed (naturally).',
+    description: 'A widget for finding rhyming words. The user can put in there own word or have one randomly generated.',
     gif: momsSpag,
     tech: ['JavaScript','API', 'React'],
     id: 2,
   },
   {
     name: 'WOOZU SOUNDS',
-    description: 'The objective of project three was to create a MERN app with full CRUD functionality. My team created an app for a fictional festival that showed information, and allowed users to comment on the stages and register their attendance. I’m particularly proud of the profile page which used all three schemas to compile the user’s activity on the app, and we implemented the Cloudinary API for profile picture uploads. Areas I worked hardest on were: authentication of the user in the back-end, consuming our API in the profile page,  and the register functionality. I also drove the concepts for the UI.',
+    description: 'The website for a fictional festival in a nearby galaxy. Users can see the line-up comment, like, and register their attendance.',
     gif: woozu,
     tech: ['MongoDB','SASS','MERN Stack'],
     id: 3,
-  },
-  {
-    name: 'Atmos',
-    description: 'Atmos is an app that allows users to pair custom audio with a photo, to create bespoke atmospheres.This is a React app consuming data from a Postgres database built by Python Django API and Django REST Framework. There are multiple relationships between three schemas, providing full CRUD functionality and complex filtering through tags attached to each atmosphere. The audio processing is based in the tone.js library, which takes a modular approach to manipulation, allowing me to invent my own sound effects.',
-    gif: atmos,
-    tech: ['Django','PostgreSQL','React','Python'],
-    id: 4,
   }
 ]
